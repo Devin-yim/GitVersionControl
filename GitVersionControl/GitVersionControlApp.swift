c21e0d258d4fc8ca142d76e7762df2d8282080e1
Devin <marvelini@outlook.com>
2022-10-29 11:20:24 +0800

    Initial Commit

---
+++ GitVersionControl/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  GitVersionControl
+//
+//  Created by 闫俊霖 on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
